da2d0f3f9e84e58ec766b6da5b4987ff30a68ea5
willo <will.olner@assetinsights.co.uk>
2020-04-18 19:42:16 +0100

    working golf mechanics with new perspective

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -8,11 +8,12 @@ public class FishController : MonoBehaviour
     Vector3 VWorld = new Vector3();
     Vector3 target = new Vector3();
     Vector3 HitVector = Vector3.zero;
-
     Vector3 ballStartPosition;
 
+    private Ray ray = new Ray();
+
     private Rigidbody2D rb;
-    public AnimationCurve velocityCurve;
+
 
     // Time when the movement started.
     private float startTime;
@@ -38,38 +39,61 @@ public class FishController : MonoBehaviour
     {
 
         VWorld.z = 0;
-        Debug.DrawLine(this.transform.position, VWorld);
+        target = (ballStartPosition + -HitVector);//*10;
+ 
         Debug.DrawLine(this.transform.position, target, Color.green);
-        if (rb.velocity == Vector2.zero)
-            moving = false;
-
-       /* if (moving)
+        if (moving && rb.velocity == Vector2.zero)
         {
-            moveDuration += Time.deltaTime;
-            
-           // Debug.Log(Mathf.Lerp(1.0f, 0.1f, distCovered));
-            // Distance moved equals elapsed time times speed..
-            float distCovered = (Time.time - startTime) * speed;
+            rb.angularVelocity = 0.0f;
+            moving = false;
+            //Debug.Log("stopped");
+        }
+
+        /* ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+         Debug.DrawLine(this.transform.position, this.transform.position+ray.direction, Color.blue);
+ */
+        // Cast a ray from screen point
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        // Save the info
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(transform.position, hit.point,Color.cyan);
+            HitVector = hit.point - this.transform.position;
+
+        }
+        
+
+        /* if (moving)
+         {
+             moveDuration += Time.deltaTime;
+
+            // Debug.Log(Mathf.Lerp(1.0f, 0.1f, distCovered));
+             // Distance moved equals elapsed time times speed..
+             float distCovered = (Time.time - startTime) * speed;
 
 
-           // distCovered = Mathf.LerpUnclamped(maxSpeed, 0.1f, (Time.time - startTime));
-            // Fraction of journey completed equals current distance divided by total distance.
-            float fractionOfJourney = distCovered / journeyLength;
+            // distCovered = Mathf.LerpUnclamped(maxSpeed, 0.1f, (Time.time - startTime));
+             // Fraction of journey completed equals current distance divided by total distance.
+             float fractionOfJourney = distCovered / journeyLength;
 
-            // distCovered -= Mathf.Abs(speed * (maxSpeed/dragFactor));
-            //Debug.Log(speed);
+             // distCovered -= Mathf.Abs(speed * (maxSpeed/dragFactor));
+             //Debug.Log(speed);
 
-            // Set our position as a fraction of the distance between the markers.
-            transform.position = Vector3.Lerp(ballStartPosition, target, fractionOfJourney);
-            Debug.DrawLine(ballStartPosition, target, Color.blue);
-            if (Vector3.Distance(target, this.transform.position) < 0.01f)
-            {
-                rb.velocity = Vector3.zero;
-                moving = false;
-                Debug.Log("Reached Target");
-                target = Vector3.zero;
-            }
-        }*/
+             // Set our position as a fraction of the distance between the markers.
+             transform.position = Vector3.Lerp(ballStartPosition, target, fractionOfJourney);
+             Debug.DrawLine(ballStartPosition, target, Color.blue);
+             if (Vector3.Distance(target, this.transform.position) < 0.01f)
+             {
+                 rb.velocity = Vector3.zero;
+                 moving = false;
+                 Debug.Log("Reached Target");
+                 target = Vector3.zero;
+             }
+         }*/
     }
 
     private void OnMouseDown()
@@ -79,35 +103,45 @@ public class FishController : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        if (moving)
-            return;
-        VScreen.x = Input.mousePosition.x;
-        VScreen.y = Input.mousePosition.y;
-        VScreen.z = Camera.main.transform.position.z;
-        VWorld = Camera.main.ScreenToWorldPoint(VScreen);
+/*        if (moving)
+            return;*/
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        // Save the info
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            ballStartPosition = this.transform.position;
+            Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            HitVector = hit.point - this.transform.position;
+            moving = true;
 
-        HitVector = VWorld - this.transform.position;
+        }
+
+
     }
 
     private void OnMouseUp()
     {
-        if (moving)
-            return;
-        startTime = Time.time;
-        ballStartPosition = this.transform.position;
-        HitVector = VWorld - this.transform.position;
+/*        if (moving)
+            return;*/
+        /*        startTime = Time.time;
+                ballStartPosition = this.transform.position;
+                HitVector = VWorld - this.transform.position;
 
         target = (ballStartPosition + -HitVector);//*10;
-        //target.Scale(new Vector3(2.0f, 2.0f, 2.0f));
+                //target.Scale(new Vector3(2.0f, 2.0f, 2.0f));
 
-        // Calculate the journey length.
+                // Calculate the journey length.
+                journeyLength = Vector3.Distance(target, this.transform.position);
+                speed = journeyLength/2;
+                maxSpeed = speed;
+                Debug.LogFormat("speed: {0} length: {1}", speed, journeyLength);
+
+        */
+        target = (ballStartPosition + -HitVector);//*10;
         journeyLength = Vector3.Distance(target, this.transform.position);
-        speed = journeyLength/2;
-        maxSpeed = speed;
-        Debug.LogFormat("speed: {0} length: {1}", speed, journeyLength);
-
-
-        this.GetComponent<Rigidbody2D>().AddForce(target * journeyLength * 10);
+        this.GetComponent<Rigidbody2D>().AddForce((target - this.transform.position)*journeyLength*10);
         moving = true;
     }
 
