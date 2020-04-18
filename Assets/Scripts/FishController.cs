3dfb910982dd667ca3eab08a35ff11444f47cfa1
willo <will.olner@assetinsights.co.uk>
2020-04-18 10:02:10 +0100

    working basic golf mechanics

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -6,6 +6,11 @@ public class FishController : MonoBehaviour
 {
     Vector3 VScreen = new Vector3();
     Vector3 VWorld = new Vector3();
+    Vector3 HitVector = Vector3.zero;
+
+    Vector3 ballStartPosition;
+
+    private Rigidbody2D rb;
 
     // Time when the movement started.
     private float startTime;
@@ -18,7 +23,7 @@ public class FishController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,7 +32,7 @@ public class FishController : MonoBehaviour
 
         VWorld.z = 0;
         Debug.DrawLine(this.transform.position, VWorld);
-        Debug.DrawLine(this.transform.position, -VWorld, Color.green);
+        Debug.DrawLine(this.transform.position, this.transform.position + -HitVector, Color.green);
 
         if (moving)
         {
@@ -36,11 +41,14 @@ public class FishController : MonoBehaviour
 
             // Fraction of journey completed equals current distance divided by total distance.
             float fractionOfJourney = distCovered / journeyLength;
+            //Debug.Log(fractionOfJourney);
 
             // Set our position as a fraction of the distance between the markers.
-            transform.position = Vector3.Lerp(this.transform.position, -VWorld, fractionOfJourney);
-            if (Vector3.Distance(-VWorld, this.transform.position) < 0.1f)
+            transform.position = Vector3.Lerp(ballStartPosition, ballStartPosition + -HitVector, fractionOfJourney);
+            Debug.DrawLine(ballStartPosition, ballStartPosition + -HitVector, Color.blue);
+            if (Vector3.Distance(ballStartPosition + -HitVector, this.transform.position) < 0.01f)
             {
+                rb.velocity = Vector3.zero;
                 moving = false;
                 Debug.Log("Reached Target");
             }
@@ -54,11 +62,14 @@ public class FishController : MonoBehaviour
 
     private void OnMouseDrag()
     {
-
+        if (moving)
+            return;
         VScreen.x = Input.mousePosition.x;
         VScreen.y = Input.mousePosition.y;
         VScreen.z = Camera.main.transform.position.z;
         VWorld = Camera.main.ScreenToWorldPoint(VScreen);
+
+        HitVector = VWorld - this.transform.position;
     }
 
     private void OnMouseUp()
@@ -66,12 +77,16 @@ public class FishController : MonoBehaviour
         if (moving)
             return;
         startTime = Time.time;
-
+        ballStartPosition = this.transform.position;
+        
         // Calculate the journey length.
         journeyLength = Vector3.Distance(VWorld, this.transform.position);
-        speed = VWorld.magnitude;
+        speed = journeyLength;
+        Debug.LogFormat("speed: {0} length: {1}", speed, journeyLength);
 
-        this.GetComponent<Rigidbody2D>().AddForce((VWorld.normalized*-1) * (Vector3.Distance(VWorld,this.transform.position)*10));
+        HitVector = VWorld - this.transform.position;
+
+        //this.GetComponent<Rigidbody2D>().AddForce((this.transform.position + -HitVector * -1) * (Vector3.Distance(VWorld,this.transform.position)*10));
         moving = true;
     }
 }
