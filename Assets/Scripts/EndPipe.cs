36f6a43c8d65c95a51eb8da80bee938beef258a9
willo <will.olner@assetinsights.co.uk>
2020-04-18 15:12:41 +0100

    added mini maap

---
+++ Assets/Scripts/EndPipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPipe : MonoBehaviour
+{
+
+    public Sprite emptySprite;
+    public Sprite fullSprite;
+
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("HOLE");
+        sr.sprite = fullSprite;
+        Destroy(collision.gameObject);
+
+        //TODO: Make this lerp towards top, it is effectively the "cutscene"
+        GameObject.FindGameObjectWithTag("MiniMapCamera").SetActive(false);
+        Camera.main.orthographicSize = 7;
+    }
+}
